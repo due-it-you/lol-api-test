23b96b37eeeee210fb10340e766bcd50505ce183
dueityou <dueityou@gmail.com>
2025-05-28 05:08:53 +0900

    [add] LOL APIで取得したJSONをオブジェクトに変換してPUUIDを取得

--- test_api.rb
+++ test_api.rb
@@ -2,13 +2,18 @@ require 'net/http'
 require 'uri'
 require 'json'
 require 'dotenv'
+require 'cgi'
 Dotenv.load
 
-endpoint = 'https://jp1.api.riotgames.com'
-path = '/lol/platform/v3/champion-rotations'
+game_name = CGI.escape('たかスペ')
+tag_line = CGI.escape('JP2')
+endpoint = 'https://asia.api.riotgames.com'
+path = "/riot/account/v1/accounts/by-riot-id/#{game_name}/#{tag_line}"
 api_key = ENV['API_KEY']
 
 uri = URI.parse("#{endpoint}#{path}?api_key=#{ENV['API_KEY']}")
 return_data = Net::HTTP.get(uri)
 
-puts return_data
+data = JSON.parse(return_data)
+
+puts data['puuid']

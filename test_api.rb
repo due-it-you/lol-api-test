5f44854182b5def9b09566155c3e2b84e4418f2a
dueityou <dueityou@gmail.com>
2025-05-28 04:18:43 +0900

    [add] LOL APIからのデータの取得が出来るかの確認のための処理

--- test_api.rb
+++ test_api.rb
@@ -4,4 +4,11 @@ require 'json'
 require 'dotenv'
 Dotenv.load
 
-p ENV['API_KEY']
+endpoint = 'https://jp1.api.riotgames.com'
+path = '/lol/platform/v3/champion-rotations'
+api_key = ENV['API_KEY']
+
+uri = URI.parse("#{endpoint}#{path}?api_key=#{ENV['API_KEY']}")
+return_data = Net::HTTP.get(uri)
+
+puts return_data

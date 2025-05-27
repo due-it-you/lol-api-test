580372aea0342019288695ec9fb5421630063814
dueityou <dueityou@gmail.com>
2025-05-28 06:53:22 +0900

    [add] 特定の試合内の各プレイヤーのpuuidの配列を取得

--- test_api.rb
+++ test_api.rb
@@ -5,8 +5,9 @@ require 'dotenv'
 require 'cgi'
 Dotenv.load
 
-game_name = CGI.escape('たかスペ')
-tag_line = CGI.escape('JP2')
+# puuidの取得
+game_name = CGI.escape('ヒイロ')
+tag_line = CGI.escape('JP1')
 endpoint = 'https://asia.api.riotgames.com'
 path = "/riot/account/v1/accounts/by-riot-id/#{game_name}/#{tag_line}"
 api_key = ENV['API_KEY']
@@ -16,4 +17,19 @@ return_data = Net::HTTP.get(uri)
 
 data = JSON.parse(return_data)
 
-puts data['puuid']
+# match idsの取得
+match_ids_path = "/lol/match/v5/matches/by-puuid/#{data['puuid']}/ids"
+uri = URI.parse("#{endpoint}#{match_ids_path}?api_key=#{ENV['API_KEY']}")
+return_data = Net::HTTP.get(uri)
+
+match_ids = JSON.parse(return_data)
+
+match_path = "/lol/match/v5/matches/#{match_ids.last}"
+
+uri = URI.parse("#{endpoint}#{match_path}?api_key=#{ENV['API_KEY']}")
+
+return_data = Net::HTTP.get(uri)
+
+match_data = JSON.parse(return_data)
+
+p match_data["metadata"]["participants"]
